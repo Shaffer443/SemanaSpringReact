1132244bbdd360c39ab36c59873510e8fad2679c
Rafael Gouveia <rafaelgouveiamelo@yahoo.com.br>
2022-07-11 12:19:10 -0300

    criação base do projeto e configurações

---
+++ backend/src/main/java/com/devsuperior/msmeta/MsmetaApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.msmeta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsmetaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsmetaApplication.class, args);
+	}
+
+}
